5a1b591595d0503c66bb0bd0065cdace837bffec
Brian Collins <brian.collins10s@gmail.com>
2024-12-24 09:43:51 -0600

    changed puma worksers from two to one

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,7 @@ if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
   #  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   #workers worker_count if worker_count > 1
-  workers 2
+  workers 1
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
