4d2df64b098b78bfef321945eddd7e1de977809b
Brian Collins <brian.collins@happyplaygrounds.com>
2025-11-17 10:35:20 -0600

    Disable CSRF

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,18 +3,17 @@
 class Api::V1::SessionsController < Devise::SessionsController
   respond_to :json
 
-  # API clients (curl, iOS) do NOT send CSRF tokens, so skip this.
-  skip_before_action :verify_authenticity_token, only: [:create, :destroy]
+  # 🚫 Disable CSRF checks entirely for this controller
+  skip_forgery_protection
 
-  # If you want to be explicit, you can also null out the session:
-   protect_from_forgery with: :null_session
-
+  # POST /api/v1/login
   def create
     self.resource = warden.authenticate!(auth_options)
     sign_in(resource_name, resource)
     respond_with(resource)
   end
 
+  # DELETE /api/v1/logout
   def destroy
     sign_out(resource_name)
     respond_to_on_destroy
@@ -28,7 +27,7 @@ class Api::V1::SessionsController < Devise::SessionsController
         id: resource.id,
         email: resource.email
       }
-      # JWT is already in Authorization header via devise-jwt
+      # JWT is already added to the Authorization header by devise-jwt
     }, status: :ok
   end
 
