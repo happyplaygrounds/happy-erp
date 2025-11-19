9341f62c0981b21984a428df9f8467297300f883
Brian Collins <brian.collins@happyplaygrounds.com>
2025-11-19 13:34:05 -0600

    bypass auth for testing

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::BaseController < ApplicationController
 
   before_action :set_default_format
 
+  # 👇 While testing: do NOT require login for API
+  # (If ApplicationController has authenticate_user!, this will override it)
+  skip_before_action :authenticate_user!, raise: false
+
   private
 
   def set_default_format
